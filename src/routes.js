dda95edf3f0c8e517413cdd04ecdaca95f948995
khalid <yusufkasim83@gmail.com>
2023-03-22 16:55:32 +0100

    ...

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,26 @@ import { Navigate, useRoutes } from 'react-router-dom';
 import DashboardLayout from './layouts/dashboard';
 import SimpleLayout from './layouts/simple';
 
+import LoginPage from './pages/LoginPage';
 import Page404 from './pages/Page404';
 import HomePage from './pages/HomePage';
-import HomePage2 from './pages/HomePage2';
+import CoolersPage from './pages/CoolersPage';
+import MyCoolersPage from './pages/MyCoolersPage';
+import PublicCoolerPage from './pages/PublicCoolerPage';
+import PrivateCoolerPage from './pages/PrivateCoolerPage';
+import MembersPage from './pages/MembersPage';
+import CreateCoolerPage from './pages/CreateCoolerPage';
+import PublicCoolerJoin from './pages/PublicCoolerJoin';
+import PrivateCoolerJoin from './pages/PrivateCoolerJoin';
+import RegisterPage from './pages/RegisterPage';
+import JoinCoolerPage from './pages/JoinCoolerPage';
+import InboxPage from './pages/InboxPage';
+import SettingsPage from './pages/SettingsPage';
+import FeedPage from './pages/FeedPage';
+import VideoPage from './pages/VideoPage';
+import DocsPage from './pages/DocsPage';
+import VideoDetailsPage from './pages/VideoDetailsPage';
+
 
 export default function Router() {
   const routes = useRoutes([
@@ -14,16 +31,32 @@ export default function Router() {
       element: <DashboardLayout />,
       children: [
         { element: <Navigate to="/login" />, index: true },
-        { path: 'home', element: <HomePage /> }
+        { path: 'home', element: <HomePage /> },
+        { path: 'feed', element: <FeedPage /> },
+        { path: 'video', element: <VideoPage /> },
+        { path: 'video-details', element: <VideoDetailsPage /> },
+        { path: 'docs', element: <DocsPage /> },
+        { path: 'members', element: <MembersPage /> },
+        { path: 'my-cooler', element: <MyCoolersPage /> },
+        { path: 'cooler', element: <CoolersPage /> },
+        { path: 'join-cooler', element: <JoinCoolerPage /> },
+        { path: 'chat', element: <InboxPage /> },
+        { path: 'settings', element: <SettingsPage /> },
+        // { path: 'my-cooler', element: <MyCoolersPage /> },
+        { path: 'public-cooler', element: <PublicCoolerPage /> },
+        { path: 'private-cooler', element: <PrivateCoolerPage /> },
+        { path: 'join-public-cooler', element: <PublicCoolerJoin /> },
+        { path: 'join-private-cooler', element: <PrivateCoolerJoin /> },
+        { path: 'create-cooler', element: <CreateCoolerPage /> },
       ],
     },
     {
       path: 'login',
-      element: <HomePage2 />,
+      element: <LoginPage />,
     },
     {
       path: 'register',
-      element: <HomePage2 />,
+      element: <RegisterPage />,
     },
     {
       element: <SimpleLayout />,
